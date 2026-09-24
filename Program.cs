7211215e85f8674b8551fa346b9a4d721736f7ff
MateBotond1 <mate.botond.2024d@telekimezotur.hu>
2026-09-24 12:32:11 +0200

    2. feladat

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<int> bevetel = new List<int>();
+Console.WriteLine("=== Kávézó Heti Bevételek Rögzítése ===");
+for  (int i = 0; i < 5; i++)
+{
+    Console.Write($"{i + 1}. nap bevétele (Ft)");
+    int napi=int.Parse( Console.ReadLine() );
+    bevetel.Add(napi);
+}
